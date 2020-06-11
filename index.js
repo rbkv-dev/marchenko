0981f66dc0918fbc0a6c74b4005ebfe897f86969
rbkv-dev <rbkv.dev@gmail.com>
2020-06-11 13:15:07 +0300

    make it better

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express'); 
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static('public'));
+const PORT = process.env.PORT || 80
 
-app.get('/',function(req,res) {
-  res.sendFile('index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3030, function () {
-  console.log('Example app listening on port 3030!');
-});
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html', {root: __dirname });
+})
+
+app.listen(PORT, () => {
+  console.log('Server has been started . . .');
+})
