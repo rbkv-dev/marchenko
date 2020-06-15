b948b3def1f60b2a7c4bf5450e64bdd1b3c01553
rbkv-dev <rbkv.dev@gmail.com>
2020-06-15 14:26:00 +0300

    make it better

--- public/js/lab_4.js
+++ public/js/lab_4.js
@@ -1,18 +1,33 @@
+const axios = require('axios');
+
 function fetchData(param) {
-  fetch(`https://jsonplaceholder.typicode.com/photos/${param}`, {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    }
-  })
-  .then(response => {
+
+  axios.get(`https://jsonplaceholder.typicode.com/photos/${param}`)
+  .then(function (response) {
     console.log(response.json());
     response.json()
   })
   .then(json => {
     console.log(json);
-    document.querySelector("#photo img").src=json.url
+    document.querySelector("#photo img").src = json.url
   })
+  .catch(function (error) {
+    console.log(error);
+  })
+  // fetch(`https://jsonplaceholder.typicode.com/photos/${param}`, {
+  //   method: "POST",
+  //   mode: "cors",
+  //   headers: {
+  //     "Content-Type": "application/json; charset=utf-8",
+  //   }
+  // })
+  // .then(response => {
+  //   console.log(response.json());
+  //   response.json()
+  // })
+  // .then(json => {
+  //   console.log(json);
+  //   document.querySelector("#photo img").src=json.url
+  // })
 }
 fetchData(1)
