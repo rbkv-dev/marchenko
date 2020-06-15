79819cd93b74e88fbc4e0de11cdda016df4acea2
rbkv-dev <rbkv.dev@gmail.com>
2020-06-15 14:00:41 +0300

    make it better

--- public/js/lab_4.js
+++ public/js/lab_4.js
@@ -2,11 +2,10 @@ function fetchData(param) {
   fetch(`https://jsonplaceholder.typicode.com/photos/${param}`, { mode: 'no-cors'})
     .then(response => {
       console.log(response.json());
-      
       response.json()
     })
     .then(json => {
-      console.log(json;
+      console.log(json);
       document.querySelector("#photo img").src=json.url
     })
 }
