2693d4f504648aacece83f2c2a57fbc127b04a93
rbkv-dev <rbkv.dev@gmail.com>
2020-06-15 11:01:05 +0300

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,42 @@ const PORT = process.env.PORT || 80
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendFile('public/index.html', {root: __dirname });
+  res.sendFile('public/index_ua.html', {root: __dirname });
+})
+
+app.get('/home-ua', (req, res) => {
+  res.sendFile('public/index_ua.html', {root: __dirname });
+})
+app.get('/home-en', (req, res) => {
+  res.sendFile('public/index_en.html', {root: __dirname });
+})
+
+app.get('/gonchari-ua', (req, res) => {
+  res.sendFile('public/index_en.html', {root: __dirname });
+})
+app.get('/gonchari-en', (req, res) => {
+  res.sendFile('public/index_en.html', {root: __dirname });
+})
+
+app.get('/rizbyari-ua', (req, res) => {
+  res.sendFile('public/index_en.html', {root: __dirname });
+})
+app.get('/rizbyari-en', (req, res) => {
+  res.sendFile('public/index_en.html', {root: __dirname });
+})
+
+app.get('/chynbyari-ua', (req, res) => {
+  res.sendFile('public/index_en.html', {root: __dirname });
+})
+app.get('/chynbyari-en', (req, res) => {
+  res.sendFile('public/index_en.html', {root: __dirname });
+})
+
+app.get('/cv-ua', (req, res) => {
+  res.sendFile('public/CV_ua.html', {root: __dirname });
+})
+app.get('/cv-en', (req, res) => {
+  res.sendFile('public/CV_en.html', {root: __dirname });
 })
 
 app.listen(PORT, () => {
